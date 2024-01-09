96cfb57eef58c853916465bdb5ba0bf96fb8d722
Talia <talialopezarias@gmail.com>
2024-01-09 20:45:27 +0100

    Ejercicio del 1 al 10

---
+++ REPAS/Ejercicio 3.py
@@ -0,0 +1,10 @@
+primerValor = input("Introduce un primer número")
+primerValor = int(primerValor)
+
+segundoValor = input("Introduce un segundo número")
+segundoValor = int(segundoValor)
+
+if primerValor > segundoValor:
+    print(primerValor)
+else: 
+    print(segundoValor)
