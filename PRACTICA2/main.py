c00cb0642a0d73381a1bbacc353215d13c27142e
Talia <talialopezarias@gmail.com>
2024-02-06 11:10:44 +0100

    Actualizacion

--- PRACTICA2/main.py
+++ PRACTICA2/main.py
@@ -1,9 +1,23 @@
 from create_table import crear_tabla
 from create import crear
+from delete import eliminar
+from update import actualizar
+from read import leer
 
 #Creación de tabla PELICULAS
 crear_tabla()
 
-#Creación de los valores de la tabla
+#Insertar valor de la tabla
 pelicula1 = ('1', 'Sherk', '1997', 'x', 'Y', '7.8')
 crear(pelicula1)
+
+pelicula2 = ('2', 'Sherk 2', '1995', 'x', 'Y', '6.8')
+
+#Eliminar valor de una tabla
+#eliminar(pelicula2)
+
+#Actualizar valores de la primera tabla
+#pelicula1_update = ('1', 'Sherk', '1995', 'Y', 'X', '7.8')
+
+#Leer valores de la tabla
+#leer()
